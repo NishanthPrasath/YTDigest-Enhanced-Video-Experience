f5466bab3af8c2780669660f6cc6eed816b8bffa
Subhash Chandran <shankarakumar.s@northeastern.edu>
2023-04-07 17:07:14 -0400

    Functionality added

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ import re
 import googleapiclient.discovery
 from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
+import gtts
 
 app = FastAPI()
 
@@ -29,39 +30,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class transcribe(BaseModel):
     link: str
     language: str
+
 
 class ask_GPT(BaseModel):
     question: str
     filename: str
 
+
 class videoUrl(BaseModel):
-   link: str
+    link: str
+
 
 load_dotenv()
 
 openai.api_key = os.environ['OPEN_AI_API_KEY']
 DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
-model=whisper.load_model('base')
+model = whisper.load_model('base')
 aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
+
 def transcribe_audio(audio, lang='English'):
-  # languages = {"af_za": "Afrikaans", "am_et": "Amharic", "ar_eg": "Arabic", "as_in": "Assamese", "az_az": "Azerbaijani", "be_by": "Belarusian", "bg_bg": "Bulgarian", "bn_in": "Bengali", "bs_ba": "Bosnian", "ca_es": "Catalan", "cmn_hans_cn": "Chinese", "cs_cz": "Czech", "cy_gb": "Welsh", "da_dk": "Danish", "de_de": "German", "el_gr": "Greek", "en_us": "English", "es_419": "Spanish", "et_ee": "Estonian", "fa_ir": "Persian", "fi_fi": "Finnish", "fil_ph": "Tagalog", "fr_fr": "French", "gl_es": "Galician", "gu_in": "Gujarati", "ha_ng": "Hausa", "he_il": "Hebrew", "hi_in": "Hindi", "hr_hr": "Croatian", "hu_hu": "Hungarian", "hy_am": "Armenian", "id_id": "Indonesian", "is_is": "Icelandic", "it_it": "Italian", "ja_jp": "Japanese", "jv_id": "Javanese", "ka_ge": "Georgian", "kk_kz": "Kazakh", "km_kh": "Khmer", "kn_in": "Kannada", "ko_kr": "Korean", "lb_lu": "Luxembourgish", "ln_cd": "Lingala", "lo_la": "Lao", "lt_lt": "Lithuanian", "lv_lv": "Latvian", "mi_nz": "Maori", "mk_mk": "Macedonian", "ml_in": "Malayalam", "mn_mn": "Mongolian", "mr_in": "Marathi", "ms_my": "Malay", "mt_mt": "Maltese", "my_mm": "Myanmar", "nb_no": "Norwegian", "ne_np": "Nepali", "nl_nl": "Dutch", "oc_fr": "Occitan", "pa_in": "Punjabi", "pl_pl": "Polish", "ps_af": "Pashto", "pt_br": "Portuguese", "ro_ro": "Romanian", "ru_ru": "Russian", "sd_in": "Sindhi", "sk_sk": "Slovak", "sl_si": "Slovenian", "sn_zw": "Shona", "so_so": "Somali", "sr_rs": "Serbian", "sv_se": "Swedish", "sw_ke": "Swahili", "ta_in": "Tamil", "te_in": "Telugu", "tg_tj": "Tajik", "th_th": "Thai", "tr_tr": "Turkish", "uk_ua": "Ukrainian", "ur_pk": "Urdu", "uz_uz": "Uzbek", "vi_vn": "Vietnamese", "yo_ng": "Yoruba"}
-  language = lang
-  options = dict(language=language, beam_size=5, best_of=5)
-  translate_options = dict(task="translate", **options)
-  translation_segments = model.transcribe(audio, **translate_options)["segments"]
-  transcript_seg=[]
-  for i in range(len(translation_segments)):
-    transcript_seg.append('[' + str(round(translation_segments[i]['start'],2))+'-'+str(round(translation_segments[i]['end'],2)) + ']' + translation_segments[i]['text'])
-  
-  return transcript_seg
+    # languages = {"af_za": "Afrikaans", "am_et": "Amharic", "ar_eg": "Arabic", "as_in": "Assamese", "az_az": "Azerbaijani", "be_by": "Belarusian", "bg_bg": "Bulgarian", "bn_in": "Bengali", "bs_ba": "Bosnian", "ca_es": "Catalan", "cmn_hans_cn": "Chinese", "cs_cz": "Czech", "cy_gb": "Welsh", "da_dk": "Danish", "de_de": "German", "el_gr": "Greek", "en_us": "English", "es_419": "Spanish", "et_ee": "Estonian", "fa_ir": "Persian", "fi_fi": "Finnish", "fil_ph": "Tagalog", "fr_fr": "French", "gl_es": "Galician", "gu_in": "Gujarati", "ha_ng": "Hausa", "he_il": "Hebrew", "hi_in": "Hindi", "hr_hr": "Croatian", "hu_hu": "Hungarian", "hy_am": "Armenian", "id_id": "Indonesian", "is_is": "Icelandic", "it_it": "Italian", "ja_jp": "Japanese", "jv_id": "Javanese", "ka_ge": "Georgian", "kk_kz": "Kazakh", "km_kh": "Khmer", "kn_in": "Kannada", "ko_kr": "Korean", "lb_lu": "Luxembourgish", "ln_cd": "Lingala", "lo_la": "Lao", "lt_lt": "Lithuanian", "lv_lv": "Latvian", "mi_nz": "Maori", "mk_mk": "Macedonian", "ml_in": "Malayalam", "mn_mn": "Mongolian", "mr_in": "Marathi", "ms_my": "Malay", "mt_mt": "Maltese", "my_mm": "Myanmar", "nb_no": "Norwegian", "ne_np": "Nepali", "nl_nl": "Dutch", "oc_fr": "Occitan", "pa_in": "Punjabi", "pl_pl": "Polish", "ps_af": "Pashto", "pt_br": "Portuguese", "ro_ro": "Romanian", "ru_ru": "Russian", "sd_in": "Sindhi", "sk_sk": "Slovak", "sl_si": "Slovenian", "sn_zw": "Shona", "so_so": "Somali", "sr_rs": "Serbian", "sv_se": "Swedish", "sw_ke": "Swahili", "ta_in": "Tamil", "te_in": "Telugu", "tg_tj": "Tajik", "th_th": "Thai", "tr_tr": "Turkish", "uk_ua": "Ukrainian", "ur_pk": "Urdu", "uz_uz": "Uzbek", "vi_vn": "Vietnamese", "yo_ng": "Yoruba"}
+    language = lang
+    options = dict(language=language, beam_size=5, best_of=5)
+    translate_options = dict(task="translate", **options)
+    translation_segments = model.transcribe(
+        audio, **translate_options)["segments"]
+    transcript_seg = []
+    for i in range(len(translation_segments)):
+        transcript_seg.append('[' + str(round(translation_segments[i]['start'], 2))+'-'+str(
+            round(translation_segments[i]['end'], 2)) + ']' + translation_segments[i]['text'])
+
+    return transcript_seg
+
 
 def chat(inp, ques, message_history, role="user"):
-
     """Question to be asked to the chatgpt api
     Args:
         inp (str): Input from the user
@@ -86,13 +94,14 @@ def chat(inp, ques, message_history, role="user"):
     reply_content = completion.choices[0].message.content
 
     # Append the generated response to the message history
-    message_history.append({"role": "assistant", "content": f"{reply_content}"})
+    message_history.append(
+        {"role": "assistant", "content": f"{reply_content}"})
 
     # Return the generated response.
     return reply_content
 
-def count_tokens(text):
 
+def count_tokens(text):
     """Count the number of tokens in a text
     Args:
         text (str): Text to count tokens for
@@ -109,86 +118,95 @@ def break_up_file_to_chunks(text, chunk_size=2000, overlap=100):
     encoding = tiktoken.get_encoding("gpt2")
     tokens = encoding.encode(text)
     num_tokens = len(tokens)
-    
+
     chunks = []
     for i in range(0, num_tokens, chunk_size - overlap):
         chunk = tokens[i:i + chunk_size]
         chunks.append(chunk)
-    
+
     return chunks
 
+
 def split_tokens_chat(transcript_seg, type):
-  prompt_response = []
-  encoding = tiktoken.get_encoding("gpt2")
-  chunks = break_up_file_to_chunks(transcript_seg)
+    prompt_response = []
+    encoding = tiktoken.get_encoding("gpt2")
+    chunks = break_up_file_to_chunks(transcript_seg)
 
-  for i, chunk in enumerate(chunks):
+    for i, chunk in enumerate(chunks):
 
-      prompt_request = "Summarize this: " + encoding.decode(chunks[i])
-      messages = [{"role": "system", "content": "This is text summarization."}]    
-      messages.append({"role": "user", "content": prompt_request})
+        prompt_request = "Summarize this: " + encoding.decode(chunks[i])
+        messages = [
+            {"role": "system", "content": "This is text summarization."}]
+        messages.append({"role": "user", "content": prompt_request})
 
-      response = openai.ChatCompletion.create(
-              model="gpt-3.5-turbo",
-              messages=messages,
-              temperature=.5,
-              max_tokens=500,
-              top_p=1,
-              frequency_penalty=0,
-              presence_penalty=0
-      )
-      
-      prompt_response.append(response["choices"][0]["message"]['content'].strip())
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=.5,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
 
-  if type == 'summarize':
-    prompt_request = "Can you summarize this? " + str(prompt_response)
-  
-  if type == 'main_topics':
-    prompt_request = "What are all the main topics and their corresponding start and end time stamps in the text? \
+        prompt_response.append(
+            response["choices"][0]["message"]['content'].strip())
+
+    if type == 'summarize':
+        prompt_request = "Can you summarize this? " + str(prompt_response)
+
+    if type == 'main_topics':
+        prompt_request = "What are all the main topics and their corresponding start and end time stamps in the text? \
      follow this format: Main topic name followed by a comma then start time followed by a comma  not hyphen then end time followed by ; not new line dont use any other characters " + str(prompt_response)
+        
+    if type == 'sentiment':
+        prompt_request = "Analyze the sentiment of the comments of the YouTube video. Provide a summary of the overall sentiment." + str(prompt_response)
 
-  response = openai.Completion.create(
-          model="text-davinci-003",
-          prompt=prompt_request,
-          temperature=.5,
-          max_tokens=1000,
-          top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0
-      )
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt_request,
+        temperature=.5,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
 
-  reply = response["choices"][0]["text"].strip()
-  return reply
+    reply = response["choices"][0]["text"].strip()
+    return reply
+
 
 def convert_text(text):
-  names = []
-  start_times = []
-  end_times = []
+    names = []
+    start_times = []
+    end_times = []
 
-  # Split the data into entries using semicolons
-  entries = text.split(';')
+    # Split the data into entries using semicolons
+    entries = text.split(';')
 
-  for entry in entries:
-      if entry.strip() == '':
-          continue
-      # Split each entry using commas
-      segments = entry.split(',')
+    for entry in entries:
+        if entry.strip() == '':
+            continue
+        # Split each entry using commas
+        segments = entry.split(',')
 
-      # Append the respective values to their lists
-      names.append(segments[0].strip())
-      start_times.append(float(segments[1].strip())/60)
-      
-      # Remove any trailing periods from the end time and convert it to a float
-      end_time = segments[2].strip()
-      if end_time.endswith('.'):
-          end_time = end_time[:-1]
-      end_times.append(float(end_time)/60)
+        # Append the respective values to their lists
+        names.append(segments[0].strip())
+        start_times.append(float(segments[1].strip())/60)
 
-  return names, start_times, end_times
+        # Remove any trailing periods from the end time and convert it to a float
+        end_time = segments[2].strip()
+        if end_time.endswith('.'):
+            end_time = end_time[:-1]
+        end_times.append(float(end_time)/60)
+
+    return names, start_times, end_times
+
 
 @app.post('/ask_GPT')
-def ask_GPT(ask_gpt:ask_GPT):
-    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+async def ask_GPT(ask_gpt: ask_GPT):
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
     bucket_name = 'damg7245-assignment-1'
     file_name = ask_gpt.filename + '.txt'
     file_key = 'Processed_file/' + file_name
@@ -201,23 +219,27 @@ def ask_GPT(ask_gpt:ask_GPT):
 
     num_tokens = count_tokens(transcript_seg)
     if num_tokens > 2000:
-        reply = split_tokens_chat(transcript_seg, ask_gpt.question)
+        reply = split_tokens_chat(
+            transcript_seg, ask_gpt.question)
 
     else:
         message_history = []
         # reply = chat(transcript_seg, "What are all the main topics and their corresponding start and end time stamps in the text?",message_history )
-        reply = chat(transcript_seg, ask_gpt.question ,message_history)
-    
-    data = {'data':reply, 'message': 'Question answered successfully', 'status_code': '200'}
+        reply = chat(transcript_seg, ask_gpt.question, message_history)
+
+    data = {'data': reply, 'message': 'Question answered successfully',
+            'status_code': '200'}
 
     return data
 
+
 @app.post('/transcribeAndStore')
-async def transcribeAndStore(transcribe_model:transcribe):
+async def transcribeAndStore(transcribe_model: transcribe):
     print("Transcribe Endpoint Called")
 
     # Set up the AWS credentials
-    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
     bucket_name = 'damg7245-assignment-1'
 
     yt = YouTube(transcribe_model.link)
@@ -231,13 +253,14 @@ async def transcribeAndStore(transcribe_model:transcribe):
     os.remove(audio_file)
     print("Audio file downloaded successfully")
 
+    input_data = open("audio.mp3", 'rb')
 
-    input_data = open("audio.mp3",'rb')
-
-    if transcribe_model.language=="English" or transcribe_model.language=="english":
-        transcription = openai.Audio.transcribe(model="whisper-1", file=input_data, response_format='text')
+    if transcribe_model.language == "English" or transcribe_model.language == "english":
+        transcription = openai.Audio.transcribe(
+            model="whisper-1", file=input_data, response_format='text')
     else:
-        transcription = openai.Audio.translate(model="whisper-1", file=input_data, response_format='text')
+        transcription = openai.Audio.translate(
+            model="whisper-1", file=input_data, response_format='text')
 
     print("Transcription completed successfully")
     outputFile = 'audio.txt'
@@ -253,20 +276,22 @@ async def transcribeAndStore(transcribe_model:transcribe):
     data = {'message': 'File uploaded successfully', 'status_code': '200'}
     return data
 
+
 @app.post('/sentimentAnalysis')
-def sentiment_analysis(url:videoUrl):
+async def sentiment_analysis(url: videoUrl):
     video_url = url.link
     YOUTUBE_API_SERVICE_NAME = "youtube"
     YOUTUBE_API_VERSION = "v3"
     youtube = googleapiclient.discovery.build(
-    YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+        YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
     # Extract the video ID from the URL
     video_id = re.findall(r"(?<=v=)[\w-]+|(?<=be/)[\w-]+", video_url)[0]
 
     # Call the YouTube API to retrieve the comments of the video
-    message_history = []
-    results = youtube.commentThreads().list(part="snippet", videoId=video_id, textFormat="plainText", maxResults=25).execute()
+    # message_history = []
+    results = youtube.commentThreads().list(part="snippet", videoId=video_id,
+                                            textFormat="plainText", maxResults=100).execute()
 
     # Extract the comments and concatenate them into a single string
     comments = ""
@@ -274,16 +299,55 @@ def sentiment_analysis(url:videoUrl):
         comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
         comments += comment + " "
 
-    message_history.append({"role": "user", "content": f"Analyze the sentiment of the comments of the YouTube video with ID {video_id}. The comments are:\n{comments}\nProvide a summary of the overall sentiment."})
+    num_tokens = count_tokens(comments)
+    question = 'Analyze the sentiment of the comments of the YouTube video. Provide a summary of the overall sentiment.'
+    if num_tokens > 2000:
+        reply = split_tokens_chat(comments, "sentiment")
+    else:
+        message_history = []
+        # reply = chat(transcript_seg, "What are all the main topics and their corresponding start and end time stamps in the text?",message_history )
+        reply = chat(comments, question, message_history)
 
-    # Generate a chat response using the OpenAI API
-    completion = openai.ChatCompletion.create( model="gpt-3.5-turbo", messages=message_history)
-
-    # Grab just the text from the API completion response
-    reply_content = completion.choices[0].message.content
-
-    # Append the generated response to the message history
-    message_history.append({"role": "assistant", "content": f"{reply_content}"})
-    data = {'data':message_history[1]['content'].strip(), 'message': 'Sentiment analysis done successfully', 'status_code': '200'}
+    data = {'data': reply, 'message': 'Sentiment analysis done successfully',
+            'status_code': '200'}
 
     return data
+
+@app.post('/textToSpeech')
+async def TextToSpeech(transcribe_model: transcribe):
+    print("Transcribe Endpoint Called")
+
+    # Set up the AWS credentials
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
+    bucket_name = 'damg7245-assignment-1'
+
+    yt = YouTube(transcribe_model.link)
+
+    audio = yt.streams.filter(only_audio=True).first()
+
+    audio_file = audio.download(filename_prefix="audio_")
+
+    mp3_file = os.path.join(os.getcwd(), "audio.mp3")
+    AudioFileClip(audio_file).write_audiofile(mp3_file)
+    os.remove(audio_file)
+    print("Audio file downloaded successfully")
+
+    input_data = open("audio.mp3", 'rb')
+
+    if transcribe_model.language == "English" or transcribe_model.language == "english":
+        transcription = openai.Audio.transcribe(
+            model="whisper-1", file=input_data, response_format='text')
+    else:
+        transcription = openai.Audio.translate(
+            model="whisper-1", file=input_data, response_format='text')    
+    print("Transcription completed successfully")
+
+    tts = gtts.gTTS(transcription, lang='en')
+    tts.save('audio_translated.mp3')
+
+    data = {'message': 'Audio translated successfully','status_code': '200'}
+
+    return data
+
+
--- pages/Home.py
+++ pages/Home.py
@@ -36,70 +36,56 @@ if video_url:
 
 
 selected_option = st.selectbox("Select an option", ["Select", "Summarizer", "Text to Speech", "Viewers Sentiments"])
-language = st.selectbox("Language", ["English", "Others"])
+#language = st.selectbox("Language", ["English", "Others"])
+language = st.selectbox("Select an Language", ["English", "Afrikaans", "Amharic", "Arabic", "Assamese", "Azerbaijani", "Belarusian", "Bulgarian", "Bengali", "Bosnian", "Catalan", "Chinese", "Czech", "Welsh", "Danish", "German", "Greek", "Spanish", "Estonian", "Persian", "Finnish", "Tagalog", "French", "Galician", "Gujarati", "Hausa", "Hebrew", "Hindi", "Croatian", "Hungarian", "Armenian", "Indonesian", "Icelandic", "Italian", "Japanese", "Javanese", "Georgian", "Kazakh", "Khmer", "Kannada", "Korean", "Luxembourgish", "Lingala", "Lao", "Lithuanian", "Latvian", "Maori", "Macedonian", "Malayalam", "Mongolian", "Marathi", "Malay", "Maltese","Myanmar", "Norwegian", "Nepali", "Dutch", "Occitan", "Punjabi", "Polish", "Pashto", "Portuguese", "Romanian", "Russian", "Sindhi", "Slovak", "Slovenian", "Shona", "Somali", "Serbian", "Swedish", "Swahili", "Tamil", "Telugu", "Tajik", "Thai", "Turkish", "Ukrainian", "Urdu", "Uzbek", "Vietnamese","Yoruba" ])
 
-if selected_option == "Summarizer":
-    st.subheader('Summarizer')
-    data = {
-                "link": video_url,
-                "language": language
-                }
-    res = requests.post('http://localhost:8000/transcribeAndStore',data=data)
-    print(res)
-    response = res.json()
-    print(response)
-
-    if response["status_code"] == 200:
+if st.button('Submit'):
+    if selected_option == "Summarizer":
+        st.subheader('Summarizer')
         data = {
-                "question": "Can you summarize?",
-                "filename": "audio"
-                }
-        res1 = requests.post('http://localhost:8000/ask_GPT',data=data)
-        print(res1)
-        response1 = res1.json()
-        print(response1)
-        if response1["status_code"] == 200:
-            st.write(response1["data"])
+                    "link": video_url,
+                    "language": language
+                    }
+        with st.spinner('Summarizing...'):
+            res = requests.post('http://localhost:8000/transcribeAndStore',json=data)
+            response = res.json()
+
+            if response["status_code"] == 200 or response["status_code"] == '200':
+                data = {
+                        "question": "summarize",
+                        "filename": "audio"
+                        }
+                res1 = requests.post('http://localhost:8000/ask_GPT',json=data)
+                response1 = res1.json()
+                if response1["status_code"] == 200 or response1["status_code"] == '200':
+                    st.write(response1["data"])
 
 
-if selected_option == "Topic Generator":
-    st.subheader('Topic Generator')
-    data = {
-                "link": video_url,
-                "language": language
-                }
-    res = requests.post('http://localhost:8000/transcribeAndStore',data=data)
-    print(res)
-    response = res.json()
-    print(response)
-    if response["status_code"] == 200:
+ 
+
+
+    if selected_option == "Viewers Sentiments":
+        st.subheader('Viewers Sentiments')
         data = {
-                "question": "What are the main topics discussed?",
-                "filename": "audio"
-                }
-        res1 = requests.post('http://localhost:8000/ask_GPT',data=data)
-        response1 = res1.json()
-        if response1["status_code"] == 200:
-            st.write(response1["data"])
+                    "link": video_url
+                    }
+        
+        with st.spinner('Analyzing user comments'):
+            res = requests.post('http://localhost:8000/sentimentAnalysis',json=data)
+            response = res.json()
 
+            if response["status_code"] == 200 or response["status_code"] == '200':
+                st.write(response["data"])
 
-if selected_option == "Viewers Sentiments":
-    st.subheader('Viewers Sentiments')
+    if selected_option == "Text to Speech":
+        data = {
+                    "link": video_url,
+                    "language": language
+                    }
+        with st.spinner('Translating language to english...'):
+            res = requests.post('http://localhost:8000/textToSpeech',json=data)
+            response = res.json()
 
+            if response["status_code"] == 200 or response["status_code"] == '200':
+                st.audio('audio_translated.mp3')
 
-if selected_option == "Custom":
-    st.subheader('Custom')
-
-
-
-
-# # Add a text input
-# search_query = st.text_input('Search on YouTube')
-
-# # Add a button
-# if st.button('Search'):
-#     st.write(f'Searching for "{search_query}"...')
-
-# # Add a video player
-# video_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-# st.video(video_url)
